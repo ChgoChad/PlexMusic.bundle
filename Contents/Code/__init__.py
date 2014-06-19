1bb45bc9f221b4f4f63d976f63c8fcd5cba1be94
mmccurdy07 <mmccurdy@gmail.com>
2014-06-19 00:44:42 -0700

    Add artist metadata, artist and album art.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -37,7 +37,15 @@ class GracenoteArtistAgent(Agent.Artist):
 
   def update(self, metadata, media, lang):
 
-    # TODO: Artist-level metadata.
+    # Fetch the first Album to use for Artist data.
+    res = XML.ElementFromURL('http://127.0.0.1:32400/services/gracenote/update?guid=' + String.URLEncode(media.children[0].guid))
+    metadata.title = res.xpath('//Directory[@type="album"]')[0].get('parentTitle')
+    metadata.summary = res.xpath('//Directory[@type="album"]')[0].get('parentSummary')
+
+    # Fetch the art if we have it.
+    for i in range(int(res.xpath('//Directory[@type="album"]')[0].get('artistArtCount'))):
+      image = HTTP.Request('http://127.0.0.1:32400/services/gracenote/thumb?guid=%s&type=artist&ord=%d' % (String.URLEncode(media.children[0].guid), i+1)).content
+      metadata.posters[i] = Proxy.Media(image, sort_order=i)
 
     for album in media.children:
 
@@ -53,6 +61,11 @@ class GracenoteArtistAgent(Agent.Artist):
       a.originally_available_at = Datetime.ParseDate(res.xpath('//Directory[@type="album"]')[0].get('year'))
       genres = [genre.get('tag') for genre in res.xpath('//Genre')]
 
+      # Fetch the art if we have it.
+      for i in range(int(res.xpath('//Directory[@type="album"]')[0].get('albumArtCount'))):
+        image = HTTP.Request('http://127.0.0.1:32400/services/gracenote/thumb?guid=%s&type=album&ord=%d' % (String.URLEncode(album.guid), i+1)).content
+        a.posters[i] = Proxy.Media(image, sort_order=i)
+
       # Add the tracks.
       for track in res.xpath('//Track'):
         
@@ -61,7 +74,7 @@ class GracenoteArtistAgent(Agent.Artist):
         
         t.index = int(i)
         t.title = track.get('title')
-        t.tempo = int(track.get('bpm'))
+        t.tempo = int(track.get('bpm') or 0)
         
         # Genres get added at the track level.
         for genre in genres:
