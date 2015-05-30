9d368edd08f7bfd3e4e93247385f07aa1806e1c7
mmccurdy07 <mmccurdy@gmail.com>
2015-05-29 17:45:13 -0700

    Search for artists on htbackdrops if MBID lookup fails.

--- Contents/Code/Artist.py
+++ Contents/Code/Artist.py
@@ -15,7 +15,8 @@ MB_NS = {'a': 'http://musicbrainz.org/ns/mmd-2.0#'}
 MB_HEADERS = {'User-Agent':'Plex Music Agent/1.0 (http://plex.tv)'}
 
 HTBACKDROPS_API_KEY = '15f8fe4ad7760d77c85e686eefafd26f'
-HTBACKDROPS_SEARCH_URL = 'http://htbackdrops.org/api/%s/searchXML?mbid=%%s&default_operator=and&limit=50&aid=1' % HTBACKDROPS_API_KEY
+HTBACKDROPS_MBID_SEARCH_URL = 'http://htbackdrops.org/api/%s/searchXML?mbid=%%s&default_operator=and&limit=50&aid=1' % HTBACKDROPS_API_KEY
+HTBACKDROPS_KEYWORD_SEARCH_URL = 'http://htbackdrops.org/api/%s/searchXML?keywords=%%s&default_operator=and&limit=50&aid=1,5' % HTBACKDROPS_API_KEY
 HTBACKDROPS_THUMB_URL = 'http://htbackdrops.org/api/%s/download/%%s/thumbnail' % HTBACKDROPS_API_KEY
 HTBACKDROPS_FULL_URL = 'http://htbackdrops.org/api/%s/download/%%s/fullsize' % HTBACKDROPS_API_KEY
 
@@ -37,7 +38,11 @@ def find_lastfm_events(artist, lang):
     
 def find_lastfm_album(artist, album, year, lang):
     try: return Core.messaging.call_external_function('com.plexapp.agents.lastfm', 'MessageKit:AlbumSearch', kwargs = dict(artist=artist['name'], album=album, year=year, lang=lang))
-    except: return None  
+    except: return None
+
+def find_htbackdrops_artist(artist):
+    try: return Core.messaging.call_external_function('com.plexapp.agents.htbackdrops', 'MessageKit:ArtistSearch', kwargs = dict(artist_name=artist))
+    except: return None
 
 def artist_mbid_lookup(artist, lastfm_artist):
   artist_mbid = None
@@ -95,10 +100,25 @@ def find_artist_art(arts, artist, lastfm_artist, fanart_artist, artist_mbid):
       for art in fanart_artist['artistbackground']:
         arts.append((art['url'], FANART_TV_PREVIEW_URL % art['url']))
 
-    # HT Backdrops.
+    # HT Backdrops. Try MBID-based lookup first.
+    htbackdrops_arts = []
     if artist_mbid is not None:
       try:
-        for image_id in XML.ElementFromURL(HTBACKDROPS_SEARCH_URL % lastfm_artist['mbid']).xpath('//image/id/text()'):
-          arts.append((HTBACKDROPS_FULL_URL % image_id, HTBACKDROPS_THUMB_URL % image_id))
+        for image_id in XML.ElementFromURL(HTBACKDROPS_MBID_SEARCH_URL % lastfm_artist['mbid']).xpath('//image/id/text()'):
+          htbackdrop_arts.append((HTBACKDROPS_FULL_URL % image_id, HTBACKDROPS_THUMB_URL % image_id))
       except Exception, e:
-        Log('Error fetching artist art from HTBackdrops: ' + str(e))
+        Log('Error fetching artist art from HTBackdrops by MBID: ' + str(e))
+
+    # If we had no MBID, or we didn't find any artwork based on one, try with the regular HT Backdrops search.
+    if len(htbackdrops_arts) == 0:
+      artist_results = find_htbackdrops_artist(artist)
+      if artist_results and len(artist_results) > 0 and artist_results[0]['score'] > 98:
+        try:
+          for image_id in XML.ElementFromURL(HTBACKDROPS_KEYWORD_SEARCH_URL % artist_results[0]['id']).xpath('//image/id/text()'):
+            htbackdrops_arts.append((HTBACKDROPS_FULL_URL % image_id, HTBACKDROPS_THUMB_URL % image_id))
+        except Exception, e:
+          Log('Error fetching artist art from HTBackdrops by artist name: ' + str(e))
+
+    # Finally append any arts we found.
+    arts.extend(htbackdrops_arts)
+    
